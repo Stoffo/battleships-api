fbeb2e0f3656f8470b87f3e5f7409433e7f1de1a
stoffo <stoffo@posteo.de>
2020-04-02 01:56:21 +0200

    Add Unit Tests

---
+++ tests/Unit/Models/AbstractShipTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Unit\Models;
+
+
+use App\Contracts\ShipInterface;
+use App\Models\Battleship;
+use App\Models\Submarine;
+use InvalidArgumentException;
+use TestCase;
+
+class AbstractShipTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Submarine(1, 12, ShipInterface::DIRECTION_RIGHT);
+
+        $this->expectException(InvalidArgumentException::class);
+        new Submarine(21, 5, ShipInterface::DIRECTION_RIGHT);
+
+        $this->expectException(InvalidArgumentException::class);
+        new Battleship(1, 5, 'foo');
+    }
+}
